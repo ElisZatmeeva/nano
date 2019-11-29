e84d037f456aa33fe42f62bab67826abc3a95eca
Elizaveta <ElisDesire@gmail.com>
2019-11-29 12:56:53 +0400

    task1

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-'use strict'
+"use strict"
+
+let money = +prompt("Ваш бюджет на месяц?", "");
+let time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+let propertyOne = prompt("Введите обязательную статью расходов в этом месяце", "");
+let sumOne = +prompt("Во сколько обойдется?", "");
+
+let propertyTwo = prompt("Введите обязательную статью расходов в этом месяце", "");
+let sumnTwo = +prompt("Во сколько обойдется?", "");
+
+let appData = {
+    "expenses": {
+        [propertyOne]: sumOne,
+        [propertyTwo]: sumnTwo,
+
+    },
+    "optionalExpenses": {},
+    "income": [],
+    "savings": false,
+    "budget": money,
+    "timeData": time,
+};
+
+let messange = money/30;
+alert( "Ваш бюджет на день:" + messange );
+
