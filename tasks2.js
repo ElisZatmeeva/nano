2af0a2275878dca2d56b6c6bdf1ec393ca4b8b39
Elizaveta <ElisDesire@gmail.com>
2019-12-04 12:51:23 +0400

    tasks 2

---
+++ tasks2.js
@@ -0,0 +1,19 @@
+'use strict'
+let i  = 0;
+while (i < 7) {
+    i++;
+    if (i == 6) {
+        continue;
+    }
+    console.log(i);
+}
+
+
+i = 0;
+do {
+ i++;
+    if (i == 6) {
+        continue;
+    }
+ console.log(i); 
+} while (i < 7)
