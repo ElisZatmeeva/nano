e9acd3f2725d7c99e0d0e1569a0a8b1fb5beffe0
Elizaveta <ElisDesire@gmail.com>
2019-12-23 12:24:11 +0400

    timer

--- tasks9/js/script.js
+++ tasks9/js/script.js
@@ -1,8 +1,11 @@
-window.addEventListener('DOMContentLoaded', function(){
-    let info = document.querySelector('.info-header'),
-        tab = document.querySelectorAll('.info-header-tab'),
-        tabContent = document.querySelectorAll('.info-tabcontent');
+window.addEventListener('DOMContentLoaded', function() {
+    
+    'use strict';
 
+        let info = document.querySelector('.info-header'),
+            tab = document.querySelectorAll('.info-header-tab'),
+            tabContent = document.querySelectorAll('.info-tabcontent');
+    
     function hideTabContent(a){
         for( let i =a; i < tabContent.length; i++){
             tabContent[i].classList.remove('show');
@@ -31,4 +34,57 @@ window.addEventListener('DOMContentLoaded', function(){
         }
     });
 
+    //Timer
+    
+    let deadLine = '2019-12-23';
+
+    function getTimeRemaining(endTime) {
+        let time = Date.parse(endTime) - Date.parse(new Date()),
+        seconds = Math.floor((time/1000)%60),
+        minutes = Math.floor((time/1000/60)%60),
+        hours = Math.floor((time/1000/60/60)%60);
+
+        return {
+            'total': time,
+            'seconds': seconds,
+            'minutes': minutes,
+            'hours': hours
+        };
+    }
+
+    function setClock(id, endTime){
+        let spanHours = document.querySelector(`#${id} .hours`),
+        spanMinutes = document.querySelector(`#${id} .minutes`),
+        spanSeconds = document.querySelector(`#${id} .seconds`),
+        timeIntervalFunction = setInterval(updateInterval, 1000);
+
+        function updateInterval(){
+            let t = getTimeRemaining(endTime);
+            spanHours.textContent = t.hours;
+            spanMinutes.textContent = t.minutes;
+            spanSeconds.textContent = t.seconds;
+
+            if (t.hours < 10 && t.hours >= 0) {
+                spanHours.textContent = "0" + t.hours;
+            } 
+            if (t.minutes < 10 && t.minutes >= 0) {
+                spanMinutes.textContent = "0" + t.minutes;
+                
+            } 
+            if (t.seconds < 10 && t.seconds >= 0) {
+                spanSeconds.textContent = "0" + t.seconds;
+            } 
+            
+
+            if(t.total <= 0) {
+                clearInterval(timeIntervalFunction);
+                spanHours.textContent = '00';
+                spanMinutes.textContent = '00';
+                spanSeconds.textContent = '00';
+            }
+            
+        }
+    }
+
+    setClock('timer', deadLine);
 });
